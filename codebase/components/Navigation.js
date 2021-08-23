0fdebdf5fb35b535394977d1131c8769a1600cc3
a3yu <74028906+a3yu@users.noreply.github.com>
2021-08-22 22:10:19 -0500

    Setup material-ui theme, and amplify

---
+++ codebase/components/Navigation.js
@@ -0,0 +1,51 @@
+import styles from "../styles/navigation.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/router";
+export default function Navbar() {
+  const router = useRouter();
+
+  const redirHome = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    router.push("/");
+  };
+
+  return (
+    //TODO ADD SIGN IN AND SIGN UP CAPABILITIES (RIGHT)
+    //TODO ADD LOGO TO RIGHT OF NAVBAR (LEFT)
+    <div className={styles.container}>
+      <ul className={styles.navbar}>
+        <li className={styles.logo}>
+          <img
+            src="/mr-logo.svg"
+            className={styles.svgLogo}
+            onClick={redirHome}
+          />
+        </li>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link href="/contests">
+            <a>Contests</a>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link href="/ranking">
+            <a>Ranking</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ codebase/src/theme.js
@@ -0,0 +1,86 @@
+import { createTheme } from "@material-ui/core/styles";
+import { responsiveFontSizes } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    common: { black: "#000", white: "#fff" },
+    background: { paper: "#fff", default: "#fafafa" },
+    primary: {
+      light: "rgba(217, 237, 247, 1)",
+      main: "rgba(51, 122, 183, 1)",
+      dark: "rgba(0, 0, 128, 1)",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "rgba(180, 180, 180, 1)",
+      main: "rgba(0, 0, 0, 1)",
+      dark: "rgba(104, 104, 104, 1)",
+      contrastText: "#fff",
+    },
+    error: {
+      light: "#e57373",
+      main: "rgba(208, 2, 27, 1)",
+      dark: "#d32f2f",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "rgba(0, 0, 0, 0.87)",
+      secondary: "rgba(0, 0, 0, 0.54)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+      hint: "rgba(0, 0, 0, 0.38)",
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    fontFamily: [
+      "Inter",
+      "ui-sans-serif",
+      "system-ui",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Helvetica Neue",
+      "Arial",
+      "Noto Sans",
+      "sans-serif",
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+      "Noto Color Emoji",
+    ].join(","),
+    h1: {
+      fontSize: "4rem",
+      fontWeight: 700,
+      lineHeight: 1,
+      color: "#fff",
+    },
+    h2: {
+      letterSpacing: "-.025em",
+      fontWeight: 700,
+      fontSize: "2.25rem",
+      lineHeight: 2.5,
+      color: "#fff",
+    },
+    h3: {
+      color: "#fff",
+      fontWeight: 500,
+      fontSize: "1.25rem",
+      lineHeight: 1.75,
+    },
+    body1: {
+      fontSize: "1rem",
+      lineHeight: 1.75,
+      color: "#d1d5db",
+    },
+    body2: {
+      fontSize: "1rem",
+      lineHeight: 1.75,
+      color: "#9CA3AF",
+    },
+  },
+});
+
+export default responsiveFontSizes(theme);
