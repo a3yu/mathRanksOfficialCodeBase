7beb687d30e4b11a6b92b623452126aea22d9bca
a3yu <74028906+a3yu@users.noreply.github.com>
2021-09-02 22:58:22 -0500

    Create aws-exports.tsx

---
+++ codebase/src/aws-exports.tsx
@@ -0,0 +1,24 @@
+const awsmobile = {
+  aws_project_region: "us-east-2",
+  aws_cognito_identity_pool_id:
+    "us-east-2:8dd835c1-f7a5-4159-bedf-8f948dcd5e0c",
+  aws_cognito_region: "us-east-2",
+  aws_user_pools_id: "us-east-2_6a3CHuCqx",
+  aws_user_pools_web_client_id: "74g47qkfpau18ma5anqgpe4rce",
+  oauth: {},
+  aws_cognito_login_mechanism: ["PREFERRED_USERNAME"],
+  aws_cognito_signup_attributes: ["EMAIL"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: ["SMS"],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [],
+  },
+  aws_appsync_graphqlEndpoint:
+    "https://vd65odxxvveo7jjm6y626ntuee.appsync-api.us-east-2.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-2",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-izsfbkwncfar3dwwj4qan35gxq",
+};
+
+export default awsmobile;
