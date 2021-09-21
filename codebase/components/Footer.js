88f6a98ad265f7ff9d5db28b54f078a32c7a8743
a3yu <74028906+a3yu@users.noreply.github.com>
2021-09-20 20:22:31 -0500

    src

--- codebase/components/Footer.js
+++ codebase/components/Footer.js
@@ -1,6 +1,6 @@
-import { style } from "@material-ui/system";
 import React from "react";
 import moment from "moment";
+import Link from "next/link";
 
 import styles from "../styles/Footer.module.scss";
 var a = moment().format("M/D/Y, h:mm:ss a");
@@ -9,19 +9,19 @@ function Footer() {
     <section className={styles.footer}>
       <hr className={styles.footerseperator} />
       <section className={styles.footersocialmedia}>
-        <a href="/" target="_blank" rel="noopener noreferrer">
+        <Link href="/" target="_blank" rel="noopener noreferrer">
           MathRanks LLC
-        </a>{" "}
+        </Link>{" "}
         (c) Copyright 2021 - Aedin Yu <br />
         Server Time (last refresh): {a}
         <br />
-        <a href="/" target="_blank" rel="noopener noreferrer">
+        <Link href="/" target="_blank" rel="noopener noreferrer">
           Privacy Policy
-        </a>{" "}
+        </Link>{" "}
         &{" "}
-        <a href="/" target="_blank" rel="noopener noreferrer">
+        <Link href="/" target="_blank" rel="noopener noreferrer">
           Terms and Conditions
-        </a>
+        </Link>
       </section>
     </section>
   );
