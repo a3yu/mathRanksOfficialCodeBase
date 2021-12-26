1ada35f989c70b0cbe59637bf3099337f4042eac
twoSig <74028906+a3yu@users.noreply.github.com>
2021-12-26 16:47:28 -0600

    a

--- codebase/pages/index.js
+++ codebase/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
-import Link from "next/link";
 import * as THREE from "three";
 import { useRouter } from "next/router";
 import { google } from "googleapis";
